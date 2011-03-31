cfbad85917f9c2ffa0820e84d36f1898a586dd33
Konstantin Shabanov <etehtsea@gmail.com>
2011-04-01 02:05:50 +0700

    fix for latest formtastic

--- lib/formtastic_datepicker_inputs/date_picker_input.rb
+++ lib/formtastic_datepicker_inputs/date_picker_input.rb
@@ -1,11 +1,14 @@
 module Formtastic
   module Inputs
     module DatePickerInput
+      include Formtastic::Inputs::StringInput
+
       def date_picker_input(method, options = {})
         format = options[:format] || Date::DATE_FORMATS[:default] || '%d %b %Y'
         string_input(method, date_picker_options(format, object.send(method)).merge(options))
       end
 
+      private
       # Generate html input options for the date_picker_input
       def date_picker_options(format, value = nil)
         { :input_html => { :class => 'ui-date-picker',
--- lib/formtastic_datepicker_inputs/date_time_picker_input.rb
+++ lib/formtastic_datepicker_inputs/date_time_picker_input.rb
@@ -1,11 +1,14 @@
 module Formtastic
   module Inputs
     module DateTimePickerInput
+      include Formtastic::Inputs::StringInput
+
       def datetime_picker_input(method, options = {})
         format = options[:format] || Date::DATE_FORMATS[:default] || '%d %b %Y %H:%M'
         string_input(method, datetime_picker_options(format, object.send(method)).merge(options))
       end
 
+      private
       # Generate html input options for the date_time_picker_input
       def datetime_picker_options(format, value = nil)
 	      { :wrapper_html => { :class => 'datetime' },
